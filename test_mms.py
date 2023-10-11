14d2109b2f78b3bea1c46b2b94ac86795eefe0ef
sushant-m-dev <mhambrey.sushant@gmail.com>
2023-10-11 16:39:13 -0500

    Test for tagalog language

--- test_mms.py
+++ test_mms.py
@@ -1,1 +1,15 @@
-print("Hello world")
+from transformers import VitsModel, AutoTokenizer
+import torch
+import scipy
+
+model = VitsModel.from_pretrained("facebook/mms-tts-tgl")
+tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-tgl")
+
+text = "interesado ka bang bumili ng pautang mula sa aming kumpanya sa 5% na rate ng interes?"
+inputs = tokenizer(text, return_tensors="pt")
+
+with torch.no_grad():
+    output = model(**inputs).waveform
+
+
+scipy.io.wavfile.write("techno.wav", rate=model.config.sampling_rate, data=output)
