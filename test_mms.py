97a8216a8c5dae6ae3ea207b48769e3474acb375
sushant-m-dev <mhambrey.sushant@gmail.com>
2023-10-11 16:49:10 -0500

    quick fix

--- test_mms.py
+++ test_mms.py
@@ -1,6 +1,10 @@
 import torch
 from transformers import VitsTokenizer, VitsModel, set_seed
 import scipy
+import sys,logging
+
+# Configure logging to write log messages to stdout
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 tokenizer = VitsTokenizer.from_pretrained("facebook/mms-tts-eng")
 model = VitsModel.from_pretrained("facebook/mms-tts-eng")
@@ -13,6 +17,7 @@ with torch.no_grad():
    outputs = model(**inputs)
 
 waveform = outputs.waveform[0]
+logging.info(waveform)
 
 scipy.io.wavfile.write("techno.wav", rate=model.config.sampling_rate, data=waveform)
 
