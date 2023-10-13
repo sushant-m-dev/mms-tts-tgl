820a0bcc01ba55050539e2c95f74192945567158
sushant-m-dev <mhambrey.sushant@gmail.com>
2023-10-13 09:51:17 -0500

    do tagalog model

--- test_mms.py
+++ test_mms.py
@@ -6,10 +6,10 @@ import sys,logging
 # Configure logging to write log messages to stdout
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-tokenizer = VitsTokenizer.from_pretrained("facebook/mms-tts-eng")
-model = VitsModel.from_pretrained("facebook/mms-tts-eng")
+tokenizer = VitsTokenizer.from_pretrained("facebook/mms-tts-tgl")
+model = VitsModel.from_pretrained("facebook/mms-tts-tgl")
 
-inputs = tokenizer(text="Hello - my dog is cute", return_tensors="pt")
+inputs = tokenizer(text="Kumusta , gusto mo bang marinig ang tungkol sa aming bagong pag-aalok ng pautang na higit pa sa mga rate ng merkado", return_tensors="pt")
 
 set_seed(555)  # make deterministic
 
