36b014d2333b6f514d0cdea31a66f0c42174af07
sushant-m-dev <mhambrey.sushant@gmail.com>
2023-10-16 00:01:19 -0500

    Adding server logic for tagalog

--- server.py
+++ server.py
@@ -3,40 +3,40 @@ from concurrent import futures
 import grpc
 import prompt_service_pb2
 import prompt_service_pb2_grpc
-# import torch
-# from transformers import VitsTokenizer, VitsModel
-# import scipy
+import torch
+from transformers import VitsTokenizer, VitsModel
+import scipy
 import logging
-# import sys
+import sys
 
-# Configure logging to write log messages to stdout
-# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+#Configure logging to write log messages to stdout
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-# # Initialize MMS TTS components
-# tokenizer = VitsTokenizer.from_pretrained("facebook/mms-tts-tgl")
-# model = VitsModel.from_pretrained("facebook/mms-tts-tgl")
+# Initialize MMS TTS components
+tokenizer = VitsTokenizer.from_pretrained("facebook/mms-tts-tgl")
+model = VitsModel.from_pretrained("facebook/mms-tts-tgl")
 
 class PromptService(prompt_service_pb2_grpc.PromptServiceServicer):
     def ProcessPrompt(self, request, context):
         try:
             text = request.prompt
 
-            updated_text = "So you want to name the audio as {}".format(text)
+            #updated_text = "So you want to name the audio as {}".format(text)
 
 
-            # # Convert the text prompt to audio
-            # inputs = tokenizer(text=request.prompt, return_tensors="pt")
+            # Convert the text prompt to audio
+            inputs = tokenizer(text=request.prompt, return_tensors="pt")
 
-            # with torch.no_grad():
-            #     output = model(**inputs).waveform
+            with torch.no_grad():
+                output = model(**inputs).waveform
 
-            # # Save the generated audio as a WAV file
-            # output_file = "output.wav"
-            # scipy.io.wavfile.write(output_file, rate=model.config.sampling_rate, data=output.float().numpy())
+            # Save the generated audio as a WAV file
+            output_file = "output.wav"
+            scipy.io.wavfile.write(output_file, rate=model.config.sampling_rate, data=output.float().numpy())
 
-            # Return success and the path to the generated audio file
-            return prompt_service_pb2.PromptResponse(success=True, message = updated_text)
-            # return prompt_service_pb2.PromptResponse(success=True, message="Audio generated successfully", audio_path=output_file)
+            #Return success and the path to the generated audio file
+            #return prompt_service_pb2.PromptResponse(success=True, message = updated_text)
+            return prompt_service_pb2.PromptResponse(success=True, message="Audio generated successfully", audio_path=output_file)
 
         except Exception as e:
             logging.error(f"Error generating audio: {str(e)}")
