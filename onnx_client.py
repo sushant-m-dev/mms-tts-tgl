0ff9eed274c60bb7c87356f678bafc7690aa16a2
sushant-m-dev <mhambrey.sushant@gmail.com>
2023-10-17 17:05:28 -0500

    Make onnx grpc client code as an input

--- onnx_client.py
+++ onnx_client.py
@@ -1,16 +1,22 @@
 import grpc
 from tts_pb2 import TextRequest
 from tts_pb2_grpc import TextToSpeechStub
+import logging
 
 def main():
     channel = grpc.insecure_channel('localhost:50051')
     stub = TextToSpeechStub(channel)
+    while True:
+        text_prompt = input("Enter a text prompt (or type 'exit' to quit): ")
 
-    text_prompt = "Hello, this is a test to determine if our model is working"
-    request = TextRequest(text=text_prompt)
+        if text_prompt.lower()=="exit":
+            break
+        request = TextRequest(text=text_prompt)
 
     response = stub.ConvertTextToSpeech(request)
     audio_data = response.audio
+
+    logging.info(audio_data)
 
     # Handle the audio data (e.g., save it as a WAV file or play it)
 
